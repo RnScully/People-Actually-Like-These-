9eac44f5bac622577ea940cc314becebf0f78565
RnScully <emunseelie@gmail.com>
2020-02-04 21:54:17 -0600

    working scraper that also cleansgit add g_rev_scrape.py

--- src/g_rev_scrape.py
+++ src/g_rev_scrape.py
@@ -20,6 +20,106 @@ import json
 
 from pymongo import MongoClient
 
+import pprint
+
+import bs4
+from bs4 import BeautifulSoup
+
+import re
+import numpy as np
+
+import spacy
+from spacy_langdetect import LanguageDetector
+
+
+def remove_text_inside_brackets(text, brackets="<>"):
+    count = [0] * (len(brackets) // 2) # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b: # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
+                if count[kind] < 0: # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else: # character is not a [balanced] bracket
+            if not any(count): # outside brackets
+                saved_chars.append(character)
+    return ''.join(saved_chars)
+
+
+def get_lang(words, nlp):
+    if len(words)<4:
+        lang = -1
+    else:
+        doc = nlp(words)
+        lang = doc._.language['language']
+    
+    return lang
+
+def get_text(readable):
+    try:
+        l = readable[0].contents # turns out there's only one readable in each review. Pretty good so far. 
+    except:
+        return -1
+    freetextlst = [s for s in l if "freeText" in str(s)] 
+
+    #rev_text = ' '.join(map(str, freetextlst)) includes the freetext and freetext containers, and that is..troublesome
+    rev_text = str(freetextlst[-1])
+    # bigstring = seperator.join(strlchildren)
+    review_nlp_document = remove_text_inside_brackets(rev_text)
+    
+    return review_nlp_document
+
+def str_to_rate(qual_state):
+    '''
+    a function that turns goodreads's "I liked it" or "I did not like it" star categories
+    into the numerical 1-5 rating that they visually imply. 
+    ++++++
+    Attributes
+    qual_state (list) a split string pulled from the beautiful soup output of .text on the rating object
+    ++++++
+    Returns
+    user_rating (int): 1-5 score based on NUMBER OF STARS SELECTED BY THE RATER. I honestly don't understand why that's not the output in the HTML. 
+    '''
+    if qual_state[-3:] == ['it', 'was', 'amazing']:
+        user_rating = 5
+    elif qual_state[-3:] ==['really','liked','it']:
+        user_rating = 4
+    elif qual_state[-2:] ==['liked', 'it']:
+        '''note that I belive any that include "really" will be given 4
+        before we get to this elif statement, therefore we don't need 
+        to worry about the issues of "really liked it" and "liked it"
+        overlapping'''
+        user_rating= 3
+    elif qual_state[-3:]==['it','was','ok']:
+        user_rating = 2
+    elif qual_state[-3:]==['not','like','it']:
+        user_rating = 1
+    else:
+        user_rating = 0
+    return user_rating
+
+
+def save_to_mongo(add_this, db, collection):
+    '''
+    Helperfunction that takes a list of reviews, appends them to dictonaries, and feeds them into mongodb
+    
+    Attributes:
+    add_this (dict): dictonary to add to mongo
+    db (str): specific string of the db to use
+    collection (str): string of the collection
+    '''
+    #connect to the mongo thing
+    client = MongoClient()
+    db = client['reveiws']
+    collection = db['clean']
+    print(add_this)
+    db.collection.insert_one(add_this) 
+    #add doc to mongodb
+    client.close()
 
 
 
@@ -70,8 +170,8 @@ def scoop_reviews():
         pass
     #turn the bookalikes into json lists
     print(str(len(reviews))+' got that many reviews')
-    for elements in reviews:
-        print(str(elements)[:20]) #hash this out, testing
+    #for elements in reviews:
+     #   print(str(elements)[:20]) #hash this out, testing
     jsons = [items.get_attribute('outerHTML') for items in reviews]
         
     return jsons
@@ -94,7 +194,7 @@ def each_page():
         time.sleep(2)
         new_doc =scoop_reviews()
         for i in new_doc:
-            print(i[:90])
+            #print(i[:90])
             docs.append(i)
         
         try:
@@ -183,25 +283,53 @@ def get_last_index():
     
     return samples.index(progress)
 
-def save_to_mongo(review_lst):
-    '''
-    Helperfunction that takes a list of reviews, appends them to dictonaries, and feeds them into mongodb
-    
+def clean_one(add_this,nlp):
+
+    ''' 
+    function which will use the scraper to update the mongo with properly cleaned stuff, so the scraper can clean and scrape (yay!)
+    +++++++++++
     Attributes:
-    review_lst (list):reviews
+    add_this (dict): dictonary of items to add to the mongo
+    +++++++++++
+    Returns:
+    Nothing
     '''
-    #connect to the mongo thing
-    client = MongoClient()
-    db = client['reviews']
-    gr_collection = db['book_reviews']
+   
+
+    client = MongoClient('localhost', 27017)
+    db=client['reviews'] ## Get all the mongo stuff up and running
+    coll=db['cleaned']
+    soup = BeautifulSoup(add_this, 'html.parser')
+    readable = soup.find_all(class_ = "readable")
     
-    for item in review_lst:
-        print(item[:20])
-        mongo_dict = {'book_id':str(book_id), 'reviews' : item}
-        gr_collection.insert_one(mongo_dict) 
-    #add doc to mongodb
-    #disconnect from mongo(do this over and over, don't keep mongo open, hopefully saves ram!?)
-    client.close()
+    
+    nlp_words = get_text(readable)
+    if nlp_words == -1:
+        return('no text here')
+
+    language = get_lang(nlp_words, nlp)
+    try: #some people are leaving reviews without giving the books any stars. interesting. 
+        rate_string = soup.find_all(class_ = re.compile('staticStars notranslate'))[0].attrs['title']
+        rate = str_to_rate(rate_string.split())
+    except IndexError:
+        rate = -1
+    
+    try: #catching the documents with nothing in them. 
+        title = soup.find_all(class_ = re.compile('lightGreyText'))[0].attrs['title'], ##title is likely to produce borked results. this one is just an isbn. soo..yeah
+    except IndexError:
+        title = -1
+        
+        
+    
+    formatted_dct = {'book_id' : int(book_id),
+                    'nlp_words' : get_text(readable),
+                    'rating_given' : rate,
+                    'title' : title, ##title is likely to produce borked results. this one is just an isbn. soo..yeah
+                    'user': soup.find_all(class_ = re.compile('user'))[0].attrs['href'][11:],
+                    'language' : language
+                          }
+
+    save_to_mongo(formatted_dct, db, coll)
 
 if __name__ == "__main__":
     option = webdriver.FirefoxOptions()
@@ -209,13 +337,17 @@ if __name__ == "__main__":
     print("Hello, I'm just getting everything together")
     driver =  webdriver.Firefox(options=option)
     actions = ActionChains(driver)
+    
+    nlp = spacy.load("en")#get information and make pipe for the language detection
+    nlp.add_pipe(LanguageDetector(), name="language_detector", last=True)
 
-
+    
 
 
     first_page = True
+    samples = list(np.arange(1,7000000)) # the bookreads books are...bad. 
     print('checking on the samples!')
-    samples = list(np.arange(1,70000000)) # the bookreads books are...bad. 
+
     current_index = get_last_index()
     print('everything seems to be in order.')
     print(current_index)
@@ -224,7 +356,7 @@ if __name__ == "__main__":
         print("**stretches**")
         current_index+=1
     #         current_index = samples.pop()
-        log_current_sample()
+        
         get_next_page(book_id)
         time.sleep(1)
 
@@ -243,10 +375,13 @@ if __name__ == "__main__":
         #scroll_to_bottom(1.5)  # let the website load for 1.5 secs...ugh
         print('scooping reveiws')
         review_lst = each_page()
-        save_to_mongo(review_lst)
-        #add doc to mongodb
+        for items in review_lst:
+            cleaned =clean_one(items, nlp)
+        #save_to_mongo(review_lst)
+        #add doc to mongodb # clean_one() now inserts to mongo
+        log_current_sample()
         print('book {} done, now sleeping'.format(book_id))
-        #time.sleep(1) #sleep now exists in the go to the next page step. . 
+        time.sleep(1) #sleep now exists in the go to the next page step. . 
     #end loop
 
 driver.close()
